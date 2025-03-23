6867892927ec17e3bd63a5a6bb2eead654e746a2
maciejs13 <maciej.sledzinski@cyfroweszkoly.pl>
2025-03-23 15:26:44 +0100

    Funkcja podsumowania ankiety

--- Preference Survey/MainWindow.axaml.cs
+++ Preference Survey/MainWindow.axaml.cs
@@ -1,11 +1,45 @@
+using System.Collections.Generic;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace Preference_Survey;
 
 public partial class MainWindow : Window
 {
+
     public MainWindow()
     {
         InitializeComponent();
+        
+    }
+    public void SubmitButton_Click(object? sender, RoutedEventArgs e)
+    {
+        var zainteresowania = "";
+        
+        if (Technologia.IsSelected == true)
+            zainteresowania = "Technologia";
+
+        if (Sport.IsSelected == true)
+            zainteresowania = "Sport";
+
+        if (Kultura.IsSelected == true)
+            zainteresowania = "Kultura";
+        
+        var licznik = 0;
+
+        if (CheckBox1.IsChecked == true)
+            licznik = licznik + 1;
+        
+        if (CheckBox2.IsChecked == true)
+            licznik = licznik + 1;
+        
+        if (CheckBox3.IsChecked == true)
+            licznik = licznik + 1;
+        
+        
+       
+        
+        
+        podsumowanie.Text = $"Użytkownik:{imie.Text}\n Zainteresowania: {zainteresowania}\n Liczba Odpowiedzi Tak: {licznik}";
     }
 }
